e5a5aed573030c9730fa8348eb300f17c70c83c8
Russell Perkins <dsw.ninjaboy@gmail.com>
2017-03-13 20:06:20 -0600

    removed lists intial logging

--- src/app/_components/lists/lists.component.ts
+++ src/app/_components/lists/lists.component.ts
@@ -15,7 +15,6 @@ export class ListsComponent implements OnInit {
 
   ngOnInit() {
     // get users from secure api end point
-    console.log("initalizing lists component.");
     this.listService.getLists()
     .subscribe(
       resp => {
