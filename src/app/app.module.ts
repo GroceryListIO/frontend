ba822c6262e97a2c5dfc053ca796510c12eda824
Russell Perkins <dsw.ninjaboy@gmail.com>
2017-05-29 14:26:46 -0600

    missed files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -21,8 +21,11 @@ import { ItemsComponent, EditItemDialog } from './_components/items/items.compon
 import { SidebarListsComponent } from './_components/sidebar-lists/sidebar-lists.component';
 import { ShoppinglistComponent } from './_components/shoppinglist/shoppinglist.component';
 import { GrocerylistComponent } from './_components/grocerylist/grocerylist.component';
+import { HomepageComponent } from './_pages/homepage/homepage.component';
+import { HomepageCtaComponent } from './_components/homepage-cta/homepage-cta.component';
 
 const appRoutes: Routes = [
+  { path: '', component: HomepageComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'lists', component: ListsComponent },
@@ -41,7 +44,9 @@ const appRoutes: Routes = [
     SidebarListsComponent,
     ShoppinglistComponent,
     GrocerylistComponent,
-    EditItemDialog
+    EditItemDialog,
+    HomepageComponent,
+    HomepageCtaComponent
   ],
   imports: [
     BrowserModule,
