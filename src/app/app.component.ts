2e9e8e3dde81269c23d52d473b9f1da6538a5821
TheDigitalNinja <dsw.ninjaboy@gmail.com>
2017-11-24 11:48:00 -0700

    google analytics page view tracking

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { AuthService } from './_services/auth/auth.service';
 import { FooterComponent } from './_components/footer/footer.component';
 import { SidebarListsComponent } from './_components/sidebar-lists/sidebar-lists.component';
+import { Router, NavigationEnd } from '@angular/router';
+import { isDevMode } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -10,5 +12,14 @@ import { SidebarListsComponent } from './_components/sidebar-lists/sidebar-lists
 })
 export class AppComponent {
   title = 'GroceryList.io';
-  constructor(public authService: AuthService) { }
+
+  constructor(public router: Router, public authService: AuthService) {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd && !isDevMode()) {
+        ga('set', 'page', event.urlAfterRedirects);
+        ga('send', 'pageview');
+      }
+    });
+  }
+
 }
